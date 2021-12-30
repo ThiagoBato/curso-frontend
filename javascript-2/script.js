aad1fd843f8b9304e8d167c89de24b93fd4b2ce0
ThiagoBato <thiago.ov@hotmail.com>
2021-12-30 20:21:46 -0300

    corrigindo exercício javascript-2

--- javascript-2/script.js
+++ javascript-2/script.js
@@ -1,34 +1,36 @@
 // 1. Resolva as operações:
-// 10 + 15 = 25
-// “10” + 2 = 102
-// “10” * 2 = 20
-// “10” / 3 = 3.3333333333333335
-// “10” % 3 = 1
-// 10 + true = 11
-// 10 == ”10” = true
-// 10 === “10” = false
-// 10 < 11 = true
-// 10 > 12 = false
-// 10 <= 10.1 = true
-// 10 > 9.99 = true
-// 10 != “dez” = true
-// 10 + true = 11
-// “dez” + true = deztrue
-// 10 + false = 10
-// 10 * false = 0
-// true + true = 2
-// 10++ = Error
-// 10-- = Error
-// 1 & 1 = 1
-// 1 & 0 = 0
-// 0 & 0 = 0
-// 1 & 0 = 0
-// 0 / 1 = 0
-// 5 + 5 == 10 = true
-// “5” + ”5” == 10 = false
-// “5” * 2 > 9 = true
-// (10 + 10) * 2 = 40
-// 10 + 10 * 2 = 30
+/*
+10 + 15 = 25
+“10” + 2 = 102
+“10” * 2 = 20
+“10” / 3 = 3.3333333333333335
+“10” % 3 = 1
+10 + true = 11
+10 == ”10” = true
+10 === “10” = false
+10 < 11 = true
+10 > 12 = false
+10 <= 10.1 = true
+10 > 9.99 = true
+10 != “dez” = true
+10 + true = 11
+“dez” + true = deztrue
+10 + false = 10
+10 * false = 0
+true + true = 2
+10++ = Error
+10-- = Error
+1 & 1 = 1
+1 & 0 = 0
+0 & 0 = 0
+1 & 0 = 0
+0 / 1 = 0
+5 + 5 == 10 = true
+“5” + ”5” == 10 = false
+“5” * 2 > 9 = true
+(10 + 10) * 2 = 40
+10 + 10 * 2 = 30
+*/
 
 // 2. Responda as perguntas de acordo com as variáveis.
 var branco = 'preto';
@@ -39,24 +41,19 @@ var valor = 30000;
 var prestacao = 750;
 
 // a) branco == “branco”
-let verdade = branco == 'branco' ? true : false;
-if (branco == 'branco') {
-    console.log(true);
-} else {
-    console.log(false);
-} // Resposta = false
+let verdadeiroFalso = branco == 'branco' ? true : false;
+console.log(verdadeiroFalso);
+// Resposta = false
+
 // b) branco == cinza
-if (branco == cinza) {
-    console.log(true);
-} else {
-    console.log(false);
-} // Resposta = false
+verdadeiroFalso = branco == cinza ? true : false;
+console.log(verdadeiroFalso);
+// Resposta = false
+
 // c) carro === branco
-if (carro === branco) {
-    console.log(true);
-} else {
-    console.log(false);
-} //Resposta  = true
+verdadeiroFalso = carro == branco ? true : false;
+console.log(verdadeiroFalso);
+//Resposta  = true
 
 // d) var cavalo = carro == “preto” ? “cinza” : “marron”;
 var cavalo = carro == 'preto' ? 'cinza' : 'marron';
