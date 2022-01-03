1b71214ca3ada8495c98db630d22cc3bbf93f8d4
ThiagoBato <thiago.ov@hotmail.com>
2022-01-03 20:18:07 -0300

    correcao do exercicio para mostrar o tipo de dado no resultado.

--- javascript-2/script.js
+++ javascript-2/script.js
@@ -1,35 +1,35 @@
 // 1. Resolva as operações:
 /*
 10 + 15 = 25
-“10” + 2 = 102
-“10” * 2 = 20
-“10” / 3 = 3.3333333333333335
-“10” % 3 = 1
-10 + true = 11
-10 == ”10” = true
-10 === “10” = false
-10 < 11 = true
-10 > 12 = false
-10 <= 10.1 = true
-10 > 9.99 = true
-10 != “dez” = true
-10 + true = 11
-“dez” + true = deztrue
-10 + false = 10
-10 * false = 0
-true + true = 2
+"10" + 2 = "102" typeof = string
+"10" * 2 = 20 typeof = number
+"10" / 3 = 3.3333333333333335 typeof = number
+"10" % 3 = 1 typeof = number
+10 + true = 11 typeof = number
+10 == "10" = true typeof = boolean
+10 === "10" = false typeof = boolean
+10 < 11 = true typeof = boolean
+10 > 12 = false typeof = boolean
+10 <= 10.1 = true typeof = boolean
+10 > 9.99 = true typeof = boolean
+10 != "dez" = true typeof = boolean
+10 + true = 11 typeof = number
+"dez" + true = "deztrue" typeof = string
+10 + false = 10 typeof = number
+10 * false = 0 typeof = number
+true + true = 2 typeof = number
 10++ = Error
 10-- = Error
-1 & 1 = 1
-1 & 0 = 0
-0 & 0 = 0
-1 & 0 = 0
-0 / 1 = 0
-5 + 5 == 10 = true
-“5” + ”5” == 10 = false
-“5” * 2 > 9 = true
-(10 + 10) * 2 = 40
-10 + 10 * 2 = 30
+1 & 1 = 1 typeof = number
+1 & 0 = 0 typeof = number
+0 & 0 = 0 typeof = number
+1 & 0 = 0 typeof = number
+0 / 1 = 0 typeof = number
+5 + 5 == 10 = true typeof = boolean
+"5" + "5" == 10 = false typeof = boolean
+"5" * 2 > 9 = true typeof = boolean
+(10 + 10) * 2 = 40 typeof = number
+10 + 10 * 2 = 30 typeof = number
 */
 
 // 2. Responda as perguntas de acordo com as variáveis.
