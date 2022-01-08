c8d6542e25099fe08602e5910798bb72a425dcd7
ThiagoBato <thiago.ov@hotmail.com>
2022-01-08 19:00:22 -0300

    subindo o exercicio 02

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,26 +1,53 @@
 const gulp = require('gulp');
+const { dest, series } = require('gulp');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const image = require('gulp-image');
-const { dest } = require('gulp');
+//const stripJS = require('gulp-strip-comments');
+const stripCSS = require('gulp-strip-css-comments');
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
-function tarefasCSS(cb) {
-    return gulp
-        .src(['./node_modules/bootstrap/dist/css/bootstrap.css', './node_modules/@fortawesome/fontawesome-free/css/all.css', './src/vendor/owl/css/owl.css', './src/vendor/jquery-ui/jquery-ui.css', './src/css/**/*.css']) //'./src/vendor/**/*.css'
+function tarefasCSS(callback) {
+    gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './src/vendor/owl/css/owl.css',
+        //'./src/vendor/jquery-ui/jquery-ui.css',
+        './src/css/**/*.css',
+    ])
+        .pipe(stripCSS())
         .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min' })) //libs.min.css
         .pipe(gulp.dest('./dist/css'));
+    return callback();
 }
-function tarefasJS() {
-    return gulp
-        .src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/dist/js/bootstrap.js', './node_modules/@fortawesome/fontawesome-free/js/all.js', './src/vendor/owl/js/owl.js', './src/vendor/jquery-ui/jquery-ui.js', './src/vendor/jquery-mask/jquery.mask.js', './src/js/custom.js']) //'./src/vendor/**/*.js'
+function tarefasJS(callback) {
+    gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './node_modules/@fortawesome/fontawesome-free/js/fontawesome.js',
+        './src/vendor/owl/js/owl.js',
+        //'./src/vendor/jquery-ui/jquery-ui.js',
+        './src/vendor/jquery-mask/jquery.mask.js',
+        './src/js/custom.js',
+    ]) //'./src/vendor/**/*.js'
+        .pipe(
+            babel({
+                comments: false,
+                presets: ['@babel/env'],
+            }),
+        )
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' })) //libs.min.js
         .pipe(gulp.dest('./dist/js'));
+    return callback();
 }
 function tarefasIMG() {
     return gulp
@@ -41,6 +68,36 @@ function tarefasIMG() {
         .pipe(dest('./dist/img'));
 }
 
+//POC Proof of Concept
+function tarefasHTML(callback) {
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('./dist'));
+
+    return callback();
+}
+
+//Servidor do Gulp
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+        },
+    });
+    gulp.watch('./src/**/*').on('change', process); // Faz a otimização quando alterar algo no arquivo de desenvolvimento
+    gulp.watch('./src/**/*').on('change', reload);
+});
+
+function end(callback) {
+    console.log('Tarefas Concluídas');
+    return callback();
+}
+
 exports.styles = tarefasCSS;
 exports.scripts = tarefasJS;
 exports.images = tarefasIMG;
+exports.htmlmin = tarefasHTML;
+
+const process = series(tarefasHTML, tarefasCSS, tarefasJS, end);
+
+exports.default = process;
